175b7b2a937ef386f9d0c98057660ed523bbba4d
Reza Harli <reza.harli@gmail.com>
2016-05-16 02:41:04 +0700

    edit membership model

--- application/modules/login/models/membership_model.php
+++ application/modules/login/models/membership_model.php
@@ -2,21 +2,6 @@
 
 class Membership_model extends CI_Model {
 
-	function validate()
-	{
-		$this->db->where('username', $this->input->post('username'));
-		$this->db->where('password', md5($this->input->post('password')));
-		$query = $this->db->get('membership')->row();
-
-		if($query)
-		{
-			return true;
-		} else {
-			return false;
-		}
-		
-	}
-	
 	function create_member()
 	{
 		
@@ -31,4 +16,62 @@ class Membership_model extends CI_Model {
 		$insert = $this->db->insert('membership', $new_member_insert_data);
 		return $insert;
 	}
+
+	function get_member_details($id=false)
+	{
+	    if( !$id )
+	    {
+	        // Set Active Record where to the current session's username
+	        if( $this->session->userdata('username') )
+	        {
+	            $this->db->where('username', $this->session->userdata('username'));
+	        }
+	        else
+	        {
+	            // Return a non logged in person from accessing member profile dashboard
+	            return false;
+	        }
+	    }
+	    else
+	    {
+	        // get the user by id
+	        $this->db->where('id', $id);
+	    }
+	    // Find all records that match this query
+	    $query = $this->db->get('membership');
+	    // In this case because we don't have a check set for unique username 
+	    // we will return the last user created with selected username.
+	    if($query->num_rows() > 0)
+	    {
+	        // Get the last row if there are more than one
+	        $row = $query->last_row();
+	        // Assign the row to our return array
+	        $data['id'] = $row->id;
+	        $data['first_name'] = $row->first_name;
+	        $data['last_name'] = $row->last_name;
+	        // Return the user found
+	        return $data;
+	    }
+	    else
+	    {
+	        // No results found
+	        return false;
+	    }
+	}
+
+	function validate()
+	{
+		$this->db->where('username', $this->input->post('username'));
+		$this->db->where('password', md5($this->input->post('password')));
+		$query = $this->db->get('membership')->row();
+
+		if($query)
+		{
+			return true;
+		} else {
+			return false;
+		}
+		
+	}
+
 }
